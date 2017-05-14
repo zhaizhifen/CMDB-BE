eeb768252f71c110da6e53191d8ebf23b304dcc6
fangniu <838307912@qq.com>
2017-05-14 19:53:05 +0800

    code optimization

--- src/cmdb_be/config.py
+++ src/cmdb_be/config.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class Config(object):
     def __init__(self, conf_path):
         self.conf_path = conf_path
-        self.default_path = os.path.join(BASE_DIR, 'conf/default.ini')
+        self.default_path = os.path.join(BASE_DIR, 'conf', 'default.ini')
         self.default_parser = ConfigParser.ConfigParser()
         self.default_parser.read(self.default_path)
 
@@ -42,40 +42,34 @@ class Config(object):
 
     @property
     def mysql_port(self):
-        if 'port' in self.mysql_options:
-            return self.parser.get('mysql', 'port')
-        else:
-            return self.default_parser.get('mysql', 'port')
+        return self.get_mysql_config('port')
 
     @property
     def mysql_host(self):
-        if 'host' in self.mysql_options:
-            return self.parser.get('mysql', 'host')
-        else:
-            return self.default_parser.get('mysql', 'host')
+        return self.get_mysql_config('host')
 
     @property
     def mysql_db_name(self):
-        if 'db_name' in self.mysql_options:
-            return self.parser.get('mysql', 'db_name')
-        else:
-            return self.default_parser.get('mysql', 'db_name')
+        return self.get_mysql_config('db_name')
 
     @property
     def mysql_user(self):
-        if 'user' in self.mysql_options:
-            return self.parser.get('mysql', 'user')
-        else:
-            return self.default_parser.get('mysql', 'user')
+        return self.get_mysql_config('user')
 
     @property
     def mysql_password(self):
-        if 'password' in self.mysql_options:
-            return self.parser.get('mysql', 'password')
-        else:
-            return self.default_parser.get('mysql', 'password')
+        return self.get_mysql_config('password')
 
-config = Config(os.path.join(BASE_DIR, 'conf/cmdb.ini'))
+    def get_mysql_config(self, option):
+        if option in self.mysql_options:
+            return self.parser.get('mysql', option)
+        else:
+            return self.default_parser.get('mysql', option)
+
+config = Config(os.path.join(BASE_DIR, 'conf', 'cmdb.ini'))
 
 if __name__ == '__main__':
     print config.mysql_host
+    print config.mysql_password
+    print config.mysql_port
+    print config.mysql_user
